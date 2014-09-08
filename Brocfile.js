18237ac298cbbf7df444b919da4501079f1237ec
Eugenio Depalo <eugenio@alphasights.com>
2014-09-08 09:32:42 +0100

    Add cookies to API requests

--- Brocfile.js
+++ Brocfile.js
@@ -17,5 +17,6 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 app.import('bower_components/paint/paint.scss');
+app.import('bower_components/underscore/underscore.js');
 
 module.exports = app.toTree();
--- app/adapters/application.coffee
+++ app/adapters/application.coffee
@@ -3,4 +3,10 @@
 ApplicationAdapter = DS.ActiveModelAdapter.extend
   host: PhoenixENV.APP.apiBaseURL
 
+  ajax: (url, method, options = {}) ->
+    @_super(url, method, _(options).extend(
+      xhrFields:
+        withCredentials: true
+    ))
+
 `export default ApplicationAdapter`
