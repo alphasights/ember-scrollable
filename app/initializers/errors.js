c21bd8877a13dc7c82358d585798b93d064e3f84
Matteo Depalo <matteodepalo@gmail.com>
2014-10-22 17:20:53 +0100

    Be GitHub

--- app/initializers/errors.js
+++ app/initializers/errors.js
@@ -6,7 +6,7 @@ export default {
   initialize: function() {
     Ember.RSVP.on('error', function() {
       new Messenger().post({
-        message: 'Sorry, but something went wrong.',
+        message: 'Something went wrong with that request, please try again.',
         type: 'error',
         showCloseButton: true
       });
