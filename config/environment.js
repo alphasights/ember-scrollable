81beaa679d9b31e0683388b8d4eeeabb03eda1f7
Matteo Depalo <matteodepalo@gmail.com>
2014-10-22 15:39:56 +0100

    Add phoenix.alphasights.com to the list of Raven whitelisted urls

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,11 @@ module.exports = function(environment) {
 
       raven: {
         url: 'https://665338a0e51643adb0c51f05dda5c368@app.getsentry.com/31225',
-        whitelistUrls: ['as-phoenix-production.herokuapp.com', 'as-phoenix-staging.herokuapp.com']
+        whitelistUrls: [
+          'as-phoenix-production.herokuapp.com',
+          'as-phoenix-staging.herokuapp.com',
+          'phoenix.alphasights.com'
+        ]
       }
     }
   };
