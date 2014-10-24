22bb947eef8a0951757a256880700935af10d272
Eugenio Depalo <eugenio@alphasights.com>
2014-10-24 16:47:51 +0100

    Change teams loading spinner appearance

--- app/components/as-progress-indicator.js
+++ app/components/as-progress-indicator.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['progress-indicator'],
-  layoutName: 'components/progress-indicator'
+  layoutName: 'components/progress-indicator',
+  spinnerWidth: 2,
+  spinnerLength: 3,
+  spinnerRadius: 4,
+  spinnerColor: '#000000'
 });
