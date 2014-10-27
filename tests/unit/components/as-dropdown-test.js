e442621a33c4f5df04a6565829167ba9520b4520
Matteo Depalo <matteodepalo@gmail.com>
2014-10-27 10:46:41 +0000

    Add as-dropdown tests

--- tests/unit/components/as-dropdown-test.js
+++ tests/unit/components/as-dropdown-test.js
@@ -1,21 +1,44 @@
+import Ember from 'ember';
 import {
   moduleForComponent,
   test
 } from 'ember-qunit';
 
 moduleForComponent('as-dropdown', 'AsDropdownComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
 });
 
 test('it renders', function() {
-  expect(2);
-
-  // creates the component instance
   var component = this.subject();
   equal(component._state, 'preRender');
 
-  // appends the component to the page
   this.append();
   equal(component._state, 'inDOM');
+});
+
+test('it has alignment info in the data-options property', function() {
+  var component = this.subject();
+
+  Ember.run(function(){
+    component.set('align', 'top');
+  });
+
+  equal(this.$().find('button').data('options'), 'align: top');
+});
+
+test('it has a data-dropdown id equal to the list id', function(){
+  var component = this.subject();
+  var id = `${Ember.guidFor(component)}-dropdown`;
+
+  equal(this.$().find('button').data('dropdown'), id);
+  equal(this.$().find('ul').attr('id'), id);
+});
+
+test('it has a title', function() {
+  var component = this.subject();
+
+  Ember.run(function(){
+    component.set('title', 'Test Title');
+  });
+
+  equal(this.$().find('button').attr('title'), 'Test Title');
 });
