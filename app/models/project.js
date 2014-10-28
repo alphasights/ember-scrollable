4e51023cb4eacb22397e9a288f0b145490f266bd
Eugenio Depalo <eugeniodepalo@gmail.com>
2014-10-28 10:56:20 +0000

    Show team page only if a team is selected

--- app/models/project.js
+++ app/models/project.js
@@ -22,14 +22,13 @@ export default DS.Model.extend({
   }).property('angles.@each.angleTeamMembershipsUpdatedAt'),
 
   members: function() {
-    return [this.get('analyst_1')].concat(
-      _(this.get('angles').toArray()).
-        chain().
-        map(function(a) { return a.get('members'); }).
-        flatten().
-        value()
-    ).uniq(false, function(a, b) { return a.get('id') === b.get('id'); });
-  }.property('analyst_1', 'angles.@each.membersUpdatedAt'),
+    return _(this.get('angles').toArray()).
+      chain().
+      map(function(a) { return a.get('members'); }).
+      flatten().
+      uniq(false, function(a) { return a.get('id'); }).
+      value();
+  }.property('angles.@each.membersUpdatedAt'),
 
   targetValues: (function() {
     return this.get('angleTeamMemberships').mapBy('targetValue');
