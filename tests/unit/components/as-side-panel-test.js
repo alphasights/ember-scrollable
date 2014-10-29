0b05ee7b0942ad67b856fd761595a34ce55a599a
Matteo Depalo <matteodepalo@gmail.com>
2014-10-29 10:14:08 +0000

    Add one integration test for as-side-panel
    Didn't manage to wait for transitions to end. I've added two other tests but commented them for now.

--- tests/unit/components/as-side-panel-test.js
+++ tests/unit/components/as-side-panel-test.js
@@ -12,15 +12,3 @@ test('it renders', function() {
   this.append();
   equal(component._state, 'inDOM');
 });
-
-// test('it animates into the viewport', function() {
-//   var component = this.subject();
-//
-//   equal(this.$().find('.panel').position().right, '-40%');
-// });
-
-// test('it closes', function() {
-//   var component = this.subject();
-//
-//   this.append();
-// });
