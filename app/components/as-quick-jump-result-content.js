9176c5a0da3346b8044cb497f848fb2dada8b966
Eugenio Depalo <eugenio@alphasights.com>
2014-11-10 10:27:49 +0000

    Namespace the component a bit better

---
+++ app/components/as-quick-jump-result-content.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['quick-jump-result-content'],
+  layoutName: 'components/quick-jump-result-content',
+  title: null,
+  details: null
+});
