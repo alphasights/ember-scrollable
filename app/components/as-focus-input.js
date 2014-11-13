7fbcde9f99d54f255716ff69a4dddf7921d42d13
Eugenio Depalo <eugenio@alphasights.com>
2014-11-13 16:49:22 +0000

    Remove unused component

--- app/components/as-focus-input.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.TextField.extend({
-  classNameBindings: [':focus-input'],
-
-  onDidInsertElement: function() {
-    this.$().focus();
-  }.on('didInsertElement')
-});
