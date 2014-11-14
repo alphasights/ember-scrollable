4ebc7c0af01f2463adf95be6ccd96c8fd6f2b483
Eugenio Depalo <eugenio@alphasights.com>
2014-11-14 12:31:57 +0000

    Remove duplicate error message

--- app/routes/application.js
+++ app/routes/application.js
@@ -16,12 +16,6 @@ export default Ember.Route.extend({
     error: function(error) {
       if (error.status === 401 || error.status === 404) {
         window.location.replace(config.APP.authUrl);
-      } else if (error.status === 500) {
-        new Messenger().post({
-          message: 'Something went wrong with that request, please try again.',
-          type: 'error',
-          showCloseButton: true
-        });
       } else {
         return true;
       }
