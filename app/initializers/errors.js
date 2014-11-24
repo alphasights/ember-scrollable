354b547162bcdb8c469705de6d6c86fc827446e5
Matteo Depalo <matteodepalo@gmail.com>
2014-11-24 12:13:36 +0000

    Add error handling

--- app/initializers/errors.js
+++ app/initializers/errors.js
@@ -1,15 +1,33 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default {
   name: 'errors',
 
-  initialize: function() {
+  initialize: function(container, application) {
     Ember.RSVP.on('error', function() {
       new Messenger().post({
-        message: 'Something went wrong with that request, please try again.',
+        message: 'Sorry, something went wrong.',
         type: 'error',
         showCloseButton: true
       });
     });
+
+    Ember.onerror = function(error) {
+      var applicationController, currentRoute;
+
+      try {
+        applicationController = application.__container__.lookup('controller:application');
+        currentRoute = applicationController.get('currentPath');
+      } catch(e) {}
+
+      Raven.captureException(error, {
+        currentRoute: currentRoute,
+        environment: config.environment
+      });
+
+      // without this line errors won't show up in the console
+      console.error(error);
+    };
   }
 };
