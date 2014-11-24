5271609e3d14c1c1713d65ba374594ff27e2c903
Matteo Depalo <matteodepalo@gmail.com>
2014-11-24 12:36:57 +0000

    Handle RSVP errors that are not requests

--- app/initializers/errors.js
+++ app/initializers/errors.js
@@ -1,18 +1,24 @@
 import Ember from 'ember';
 import config from '../config/environment';
 
+var initialized = false;
+
 export default {
   name: 'errors',
 
   initialize: function(container, application) {
     if (initialized) { return; }
 
-    Ember.RSVP.on('error', function() {
-      new Messenger().post({
-        message: 'Something went wrong with that request, please try again.',
-        type: 'error',
-        showCloseButton: true
-      });
+    Ember.RSVP.on('error', function(error) {
+      if (error.jqXHR != null) {
+        new Messenger().post({
+          message: 'Something went wrong with that request, please try again.',
+          type: 'error',
+          showCloseButton: true
+        });
+      } else {
+        Ember.onerror(error);
+      }
     });
 
     Ember.onerror = function(error) {
