65dc60f3a30df435c9c0d825ea20a173c6352f07
Matteo Depalo <matteodepalo@gmail.com>
2014-11-28 16:05:22 +0000

    Test side bar preferences

---
+++ tests/unit/views/sidebar-test.js
@@ -0,0 +1,25 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+import Ember from 'ember';
+
+moduleFor('view:sidebar', 'SidebarView');
+
+// Replace this with your real tests.
+test('has a default value for isCollapsed', function() {
+  var view = this.subject();
+
+  equal(view.get('isCollapsed'), false);
+});
+
+test('isCollapsed equals the sideBarCollapsed preference if set', function() {
+  var view = this.subject();
+
+  view.set('controller', Ember.Controller.create({
+    preferences: Ember.Object.create({ sideBarCollapsed: true })
+  }));
+
+  equal(view.get('isCollapsed'), true);
+});
