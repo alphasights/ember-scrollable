f1de94aeee7bf3b38e6ef5d7d79daaa3efc67cdb
Matteo Depalo <matteodepalo@gmail.com>
2014-12-01 11:50:47 +0000

    Remove deprecated view test

--- tests/unit/views/sidebar-test.js
+++ tests/unit/views/sidebar-test.js
@@ -7,18 +7,10 @@ import Ember from 'ember';
 
 moduleFor('view:sidebar', 'SidebarView');
 
-test('has a default value for isCollapsed', function() {
-  var view = this.subject();
-
-  equal(view.get('isCollapsed'), false);
-});
-
-test('isCollapsed equals the sidebarCollapsed preference if set', function() {
-  var view = this.subject();
-
-  view.set('controller', Ember.Controller.create({
+test('isCollapsed equals the sidebarCollapsed preference', function() {
+  var view = this.subject({ controller: Ember.Controller.create({
     preferences: Ember.Object.create({ sidebarCollapsed: true })
-  }));
+  })});
 
   equal(view.get('isCollapsed'), true);
 });
