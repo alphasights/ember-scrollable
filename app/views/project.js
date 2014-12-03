8e001ce45e43d16ea6d70e826a9337129f113cc4
Eugenio Depalo <eugenio@alphasights.com>
2014-12-03 14:52:01 +0000

    Bind the sidepanel action to details

--- app/views/project.js
+++ app/views/project.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  classNameBindings: [':project'],
-
-  click: function() {
-    this.get('controller').transitionToRoute('team.project', this.get('controller.id'));
-  }
+  classNameBindings: [':project']
 });
