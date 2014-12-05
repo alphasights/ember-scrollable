41a632a2ad8056e224383e136e78894a4ffcbbda
Eugenio Depalo <eugeniodepalo@gmail.com>
2014-12-05 12:37:33 +0000

    Create a project view inheriting from side panel

--- app/routes/side-panel.js
+++ app/routes/side-panel.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  viewName: 'sidePanel',
+
   renderTemplate: function() {
     this.render(this.routeName, {
       into: 'application',
       outlet: 'side-panel',
-      view: 'sidePanel'
+      view: this.viewName
     });
 
     this.render(this.sidePanelActionsTemplateName, {
--- app/routes/team/project.js
+++ app/routes/team/project.js
@@ -1,6 +1,8 @@
 import SidePanelRoute from '../side-panel';
 
 export default SidePanelRoute.extend({
+  viewName: 'team/project',
+
   sidePanelActionsTemplateName: 'team/project-side-panel-actions',
 
   actions: {
