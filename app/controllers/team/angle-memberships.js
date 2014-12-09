1ec241114f032d305c4938522bad30362557bcdd
Eugenio Depalo <eugeniodepalo@gmail.com>
2014-12-09 15:56:28 +0000

    Add progress indicator to member search

--- app/controllers/team/angle-memberships.js
+++ app/controllers/team/angle-memberships.js
@@ -1,6 +1,43 @@
 import Ember from 'ember';
+import PromiseController from '../promise';
 
 export default Ember.ArrayController.extend({
   needs: ['team'],
-  team: Ember.computed.alias('controllers.team')
+  team: Ember.computed.alias('controllers.team'),
+  requestPromise: null,
+  query: null,
+
+  members: function() {
+    if (this.get('requestPromise')) {
+      return this.get('results');
+    } else {
+      return this.get('team.members');
+    }
+  }.property('requestPromise', 'team.members'),
+
+  queryDidChange: function() {
+    if (Ember.isEmpty(this.get('query'))) {
+      this.set('requestPromise', null);
+    }
+
+    Ember.run.debounce(this, '_queryDidChange', 100);
+  }.observes('query'),
+
+  _queryDidChange: function() {
+    var query = this.get('query');
+
+    if (query && query.length > 1) {
+      var requestPromise = PromiseController.create({
+        promise: this.store.find('user', { query: query }).then(response => {
+          if (requestPromise === this.get('requestPromise')) {
+            this.set('results', response);
+          }
+        })
+      });
+
+      this.set('requestPromise', requestPromise);
+    } else {
+      this.set('results', null);
+    }
+  },
 });
