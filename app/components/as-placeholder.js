b7ff592033d240640004d4e18364a403898c1f02
Andrei Popa <andrei.deioo@gmail.com>
2014-12-09 16:14:27 +0000

    Create a Placeholder component instead of rendering the view
    This also adds a set of params we can pass for each view

--- app/components/as-placeholder.js
+++ app/components/as-placeholder.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNameBindings: [':placeholder'],
-  attributeBindings: ['icon', 'title', 'link'],
-  tagName: 'section'
+  attributeBindings: ['icon', 'sectionName', 'link'],
+  tagName: 'section',
+
+  externalUrl: 'https://secure.alphasights.com/',
+
+  icon: 'default',
+  sectionName: null,
+  link: 'system',
+
+  externalLink: function() {
+    return this.get('externalUrl') + this.get('link');
+  }.property('externalLink')
 });
