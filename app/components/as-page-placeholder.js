e948dc4819ee19b779dc926c90bc76b3aad0603b
Andrei Popa <andrei.deioo@gmail.com>
2014-12-11 15:52:17 +0000

    Remove page placeholder icon property and generate the css class based on the page name

--- app/components/as-page-placeholder.js
+++ app/components/as-page-placeholder.js
@@ -2,14 +2,17 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Component.extend({
-  classNameBindings: [':page-placeholder'],
+  classNameBindings: [':page-placeholder', 'pageCssClass'],
   tagName: 'section',
 
-  icon: 'default',
   pageName: null,
   path: 'system',
 
   externalPath: function() {
     return config.APP.pistachioUrl + '/' + this.get('path');
-  }.property('externalPath')
+  }.property('externalPath'),
+
+  pageCssClass: function() {
+    return this.get('pageName').dasherize();
+  }.property('pageCssClass')
 });
