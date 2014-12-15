ac1b3e4a14aadd0b7ee3b8f9a8c414224c9a0df3
Eugenio Depalo <eugenio@alphasights.com>
2014-12-15 15:54:10 +0000

    Remove custom pageCssClass attribute

--- app/components/as-page-placeholder.js
+++ app/components/as-page-placeholder.js
@@ -2,17 +2,13 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Component.extend({
-  classNameBindings: [':page-placeholder', 'pageCssClass'],
+  classNameBindings: [':page-placeholder'],
   tagName: 'section',
 
   pageName: null,
   path: 'system',
 
-  externalPath: function() {
+  url: function() {
     return config.APP.pistachioUrl + '/' + this.get('path');
-  }.property('externalPath'),
-
-  pageCssClass: function() {
-    return this.get('pageName').dasherize();
-  }.property('pageCssClass')
+  }.property('externalPath')
 });
