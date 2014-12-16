334208a4cec1466ab82985dcf2ffb5857ea72cfd
Eugenio Depalo <eugeniodepalo@gmail.com>
2014-12-16 13:23:56 +0000

    Make the whole project item clickable

--- app/controllers/team/project.js
+++ app/controllers/team/project.js
@@ -2,21 +2,7 @@ import Ember from 'ember';
 import config from '../../config/environment';
 
 export default Ember.ObjectController.extend({
-  progress: function() {
-    var deliveryTarget = this.get('deliveryTarget');
-
-    if (deliveryTarget === 0) {
-      return 0;
-    } else {
-      return this.get('deliveredAdvisorsCount') / deliveryTarget;
-    }
-  }.property('deliveredAdvisorsCount', 'deliveryTarget'),
-
-  members: function() {
-    return _(this.get('model.members')).without(this.get('lead'));
-  }.property('model.members.[]'),
-
-  pistachioProjectUrl: function() {
+  pistachioUrl: function() {
     return `${config.APP.pistachioUrl}/projects/${this.get('id')}`;
   }.property('id')
 });
