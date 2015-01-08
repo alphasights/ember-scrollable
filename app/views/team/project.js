4fd81d8743bea9e46e8f34e5c02afe0a81ae47cf
Matteo Depalo <matteodepalo@gmail.com>
2015-01-08 14:51:46 +0000

    Add tests for key interactions

--- app/views/team/project.js
+++ app/views/team/project.js
@@ -5,15 +5,13 @@ export default SidePanelView.extend({
   tagName: 'article',
 
   setupArrowKeysHandling: function() {
-    this.$(document).on('keyup.projectNavigation', this.arrowKeysHandler.bind(this));
+    this.$(document).on('keyup.projectNavigation', (e) => {
+      if (e.keyCode === 37) { this.get('controller').send('previous'); } // left arrow
+      if (e.keyCode === 39) { this.get('controller').send('next'); } // right arrow
+    });
   }.on('didInsertElement'),
 
   removeArrowKeysHandling: function() {
     this.$(document).off('keyup.projectNavigation');
-  }.on('willDestroyElement'),
-
-  arrowKeysHandler: function(e) {
-    if (e.keyCode === 37) { this.get('controller').send('previous'); } // left arrow
-    if (e.keyCode === 39) { this.get('controller').send('next'); } // right arrow
-  }
+  }.on('willDestroyElement')
 });
