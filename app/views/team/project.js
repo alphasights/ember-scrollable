4df02bd67a13a7b4b0d33d006838e8285b4299ca
Matteo Depalo <matteodepalo@gmail.com>
2015-01-08 14:42:14 +0000

    Allow project navigation with arrow keys

--- app/views/team/project.js
+++ app/views/team/project.js
@@ -2,5 +2,18 @@ import SidePanelView from '../side-panel';
 
 export default SidePanelView.extend({
   classNameBindings: [':project'],
-  tagName: 'article'
+  tagName: 'article',
+
+  setupArrowKeysHandling: function() {
+    this.$(document).on('keyup.projectNavigation', this.arrowKeysHandler.bind(this));
+  }.on('didInsertElement'),
+
+  removeArrowKeysHandling: function() {
+    this.$(document).off('keyup.projectNavigation');
+  }.on('willDestroyElement'),
+
+  arrowKeysHandler: function(e) {
+    if (e.keyCode === 37) { this.get('controller').send('previous'); } // left arrow
+    if (e.keyCode === 39) { this.get('controller').send('next'); } // right arrow
+  }
 });
