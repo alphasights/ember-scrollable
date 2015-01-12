312bf94b3939e0866351339bb8b48db4231a7e27
Matheus Bras <bras.matheus@gmail.com>
2015-01-12 17:26:13 -0500

    Add a select input to switch teams

--- app/controllers/team.js
+++ app/controllers/team.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  teamSelecting: false,
+  selectedTeam: null,
+
   queryParams: {
     sortPropertyId: 'sort_by'
   },
@@ -11,11 +14,20 @@ export default Ember.ObjectController.extend({
     return `${EmberENV.pistachioUrl}/whiteboard`;
   }.property(),
 
+  selectedTeamDidChange: function() {
+    this.get('controller').set('teamSelecting', false);
+    this.get('controller').transitionToRoute('team', this.get('selection.id'));
+  },
+
   actions: {
     submitFeedback: function() {
       /* jshint newcap: false */
       Intercom('showNewMessage');
       /* jshint newcap: true */
+    },
+
+    selectTeam: function() {
+      this.set('teamSelecting', true);
     }
   }
 });
