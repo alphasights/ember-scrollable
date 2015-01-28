e7c3d38d69d1314619fecc15f5704964daa4823c
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-01-28 11:43:49 +0000

    Revert work on extracting progress component

--- app/controllers/teams/team/project-list-item.js
+++ app/controllers/teams/team/project-list-item.js
@@ -6,6 +6,16 @@ export default Ember.ObjectController.extend({
   team: Ember.computed.alias('controllers.teams/team'),
   hasDeliveryTarget: Ember.computed.gt('deliveryTarget', 0),
 
+  progress: function() {
+    var deliveryTarget = this.get('deliveryTarget');
+
+    if (deliveryTarget === 0) {
+      return 0;
+    } else {
+      return this.get('deliveredAdvisorsCount') / deliveryTarget;
+    }
+  }.property('deliveredAdvisorsCount', 'deliveryTarget'),
+
   members: function() {
     return _(this.get('model.members')).without(this.get('lead'));
   }.property('model.members.[]'),
