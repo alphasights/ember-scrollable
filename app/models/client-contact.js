3d7843491d2fbf89e61c1dc650676e2f6930b9b0
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-03 15:34:05 +0000

    Add client account model

--- app/models/client-contact.js
+++ app/models/client-contact.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  clientAccount: DS.belongsTo('clientAccount'),
   interactions: DS.hasMany('interaction'),
   name: DS.attr('string'),
   phoneNumbers: DS.attr()
