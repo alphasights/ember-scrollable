c8ac5fe348da7dacd6cab8601967b47b9e9f3bf7
Andrei Popa <andrei.deioo@gmail.com>
2015-02-04 10:46:44 +0000

    Add a static avatarUrl for the clientContact model

--- app/models/client-contact.js
+++ app/models/client-contact.js
@@ -1,6 +1,9 @@
 import DS from 'ember-data';
 
 var ClientContact = DS.Model.extend({
+  avatarUrl: DS.attr('string', {
+    defaultValue: 'https://dzaqm99ko40cm.cloudfront.net/assets/avatars/default_large-7494586099c783d8e2cf6e056dc88a20.png'
+  }),
   clientAccount: DS.belongsTo('clientAccount', { async: true }),
   interactions: DS.hasMany('interaction', { async: true }),
   name: DS.attr('string'),
