34ffccfcea2eaa328b253c4c01c82865425099f7
Andrei Popa <andrei.deioo@gmail.com>
2015-02-04 15:27:41 +0000

    Add side panel actions for interactions (does not include controller actions)

--- app/views/dashboard/interaction.js
+++ app/views/dashboard/interaction.js
@@ -2,5 +2,15 @@ import SidePanelView from 'phoenix/views/side-panel';
 
 export default SidePanelView.extend({
   classNameBindings: [':interaction'],
-  tagName: 'article'
+  tagName: 'article',
+
+  keyEvents: {
+    leftArrow: function() {
+      this.get('controller').send('previous');
+    },
+
+    rightArrow: function() {
+      this.get('controller').send('next');
+    }
+  }
 });
