ae40782d37b2684962a184994c1a9a4363ab7446
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-05 14:17:02 +0000

    Add pistachio Url to interaction

--- app/models/interaction.js
+++ app/models/interaction.js
@@ -4,7 +4,11 @@ var Interaction = DS.Model.extend({
   advisor: DS.belongsTo('advisor', { async: true }),
   clientContact: DS.belongsTo('clientContact', { async: true }),
   project: DS.belongsTo('project', { async: true }),
-  scheduledCallTime: DS.attr('date')
+  scheduledCallTime: DS.attr('date'),
+
+  pistachioUrl: function() {
+    return `${EmberENV.pistachioUrl}/interactions/${this.get('id')}`;
+  }.property()
 });
 
 Interaction.reopenClass({
