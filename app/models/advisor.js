cf0a086f555a7f59e59a91a82e95ea786ca2f90c
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-05 11:11:49 +0000

    Add jobTitle and companyName to Advisor

--- app/models/advisor.js
+++ app/models/advisor.js
@@ -2,8 +2,10 @@ import DS from 'ember-data';
 
 var Advisor = DS.Model.extend({
   avatarUrl: DS.attr('string'),
+  companyName: DS.attr('string'),
   emails: DS.attr(),
   interactions: DS.hasMany('interaction', { async: true }),
+  jobTitle: DS.attr('string'),
   name: DS.attr('string'),
   phoneNumbers: DS.attr()
 });
@@ -13,13 +15,17 @@ Advisor.reopenClass({
     {
       id: 1,
       avatarUrl: 'http://www.fillmurray.com/120/120',
+      companyName: 'Ghostbusters INC.',
       emails: ['billmurray@email.com'],
+      jobTitle: 'Ghostbuster',
       name: 'Bill Murray',
       phoneNumbers: ['+123456789']
     },
     { id: 2,
       avatarUrl: 'https://placekitten.com/120/120',
+      companyName: 'Meow Mix Industries',
       emails: ['kitty_von_meow39@email.com'],
+      jobTitle: 'Chief Happiness Officer',
       name: 'Kitty Von Meow',
       phoneNumbers: ['+198765432']
     }
