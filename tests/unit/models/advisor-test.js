204bdaa09650b1033234de0bb40483e3b4e14eef
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-09 16:15:04 +0000

    Update advisor model tests

--- tests/unit/models/advisor-test.js
+++ tests/unit/models/advisor-test.js
@@ -8,12 +8,9 @@ moduleForModel('advisor', 'Advisor', {
   needs: ['model:interaction', 'model:client-contact', 'model:project']
 });
 
-test('it exists', function() {
-  var model = this.subject();
-  ok(!!model);
-});
+test('#currentPosition', function() {
+  expect(4);
 
-test('currentPosition joins jobTitle and companyName', function() {
   var model = this.subject(
     { jobTitle: 'Big Boss Man', companyName: 'LOL Incorporated' }
   );
@@ -21,16 +18,35 @@ test('currentPosition joins jobTitle and companyName', function() {
   equal(
     model.get('currentPosition'),
     'Big Boss Man at LOL Incorporated',
-    'Includes both title and company when available'
+    'returns both title and company name when available'
   );
 
   Ember.run(function() {
     model.set('companyName', '');
   });
-
   equal(
     model.get('currentPosition'),
     'Big Boss Man',
-    'Only includes job title when there is no company name'
+    'only returns job title when there is no company name'
+  );
+
+  Ember.run(function() {
+    model.set('jobTitle', '');
+    model.set('companyName', 'LOL Incorporated');
+  });
+  equal(
+    model.get('currentPosition'),
+    'LOL Incorporated',
+    'only returns company name when there is no job title'
+  );
+
+  Ember.run(function() {
+    model.set('jobTitle', '');
+    model.set('companyName', '');
+  });
+  equal(
+    model.get('currentPosition'),
+    '',
+    'returns an empty string when there is no job title or company name'
   );
 });
