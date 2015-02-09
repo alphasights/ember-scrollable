89c87bd4c1b86d9d71e3374f7d89e8f2d7e00d4c
Matteo Depalo <matteodepalo@gmail.com>
2015-02-09 12:05:32 +0000

    Update utc transform to use global moment

--- app/transforms/utc.js
+++ app/transforms/utc.js
@@ -1,12 +1,11 @@
 import DS from 'ember-data';
-import moment from 'ember-moment/helpers/moment';
 
 export default DS.Transform.extend({
   serialize: function(value) {
-    return value ? moment.utc(value) : null;
+    return value ? value.toJSON() : null;
   },
 
   deserialize: function(value) {
-    return value ? moment(value) : null;
+    return moment(value);
   }
 });
