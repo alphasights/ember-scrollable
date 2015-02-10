72b3ee8da60b6ea09622399a91f7b6374d801674
Matteo Depalo <matteodepalo@gmail.com>
2015-02-10 12:32:47 +0000

    Refactor advisor model tests

--- tests/unit/models/advisor-test.js
+++ tests/unit/models/advisor-test.js
@@ -5,48 +5,41 @@ import {
 import Ember from 'ember';
 
 moduleForModel('advisor', 'Advisor', {
-  needs: ['model:interaction', 'model:client-contact', 'model:project']
+  needs: ['model:interaction', 'model:client-contact', 'model:project'],
+
+  beforeEach: function() {
+    this.model = this.subject(
+      { jobTitle: 'Job Title', companyName: 'Company Name' }
+    );
+  }
 });
 
-test('#currentPosition', function() {
-  expect(4);
+test('#currentPosition returns both title and company name when available', function() {
+  equal(this.model.get('currentPosition'), 'Job Title at Company Name');
+});
 
-  var model = this.subject(
-    { jobTitle: 'Job Title', companyName: 'Company Name' }
-  );
-
-  equal(
-    model.get('currentPosition'),
-    'Job Title at Company Name',
-    'returns both title and company name when available'
-  );
-
-  Ember.run(function() {
-    model.set('companyName', '');
+test('#currentPosition only returns job title when there is no company name', function() {
+  Ember.run(() => {
+    this.model.set('companyName', '');
   });
-  equal(
-    model.get('currentPosition'),
-    'Job Title',
-    'only returns job title when there is no company name'
-  );
 
-  Ember.run(function() {
-    model.set('jobTitle', '');
-    model.set('companyName', 'Company Name');
-  });
-  equal(
-    model.get('currentPosition'),
-    'Company Name',
-    'only returns company name when there is no job title'
-  );
+  equal(this.model.get('currentPosition'), 'Job Title');
+});
 
-  Ember.run(function() {
-    model.set('jobTitle', '');
-    model.set('companyName', '');
+test('#currentPosition only returns company name when there is no job title', function() {
+  Ember.run(() => {
+    this.model.set('jobTitle', '');
+    this.model.set('companyName', 'Company Name');
   });
-  equal(
-    model.get('currentPosition'),
-    '',
-    'returns an empty string when there is no job title or company name'
-  );
+
+  equal(this.model.get('currentPosition'), 'Company Name');
+});
+
+test('#currentPosition returns an empty string when there is no job title or company name', function () {
+  Ember.run(() => {
+    this.model.set('jobTitle', '');
+    this.model.set('companyName', '');
+  });
+
+  equal(this.model.get('currentPosition'), '');
 });
