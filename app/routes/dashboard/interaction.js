5d987375463efe5f95d300f2438a9d94ada574d0
Eugenio Depalo <eugeniodepalo@gmail.com>
2015-02-16 12:09:02 +0000

    Temporarily removoe navigation arrows from interactions

--- app/routes/dashboard/interaction.js
+++ app/routes/dashboard/interaction.js
@@ -2,7 +2,6 @@ import SidePanelRoute from 'phoenix/routes/side-panel';
 
 export default SidePanelRoute.extend({
   viewName: 'dashboard.interaction',
-  sidePanelActionsTemplateName: 'dashboard/interaction-side-panel-actions',
 
   model: function(params) {
     var interactionId = params.interaction_id;
