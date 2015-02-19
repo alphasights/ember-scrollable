09ecdc54b6195f3482e345871493cdf5574cf143
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-19 11:46:41 -0500

    Display basic performance stats in dashboard header

--- app/models/delivery-performance.js
+++ app/models/delivery-performance.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  monthlyCreditCount: DS.attr('number'),
+  currentMonthCreditCount: DS.attr('number'),
   monthlyTarget: DS.attr('number'),
   user: DS.belongsTo('user')
 });
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
-      upcomingInteractions: this.store.find('interaction')
+      upcomingInteractions: this.store.find('interaction'),
+      deliveryPerformance: this.store.find('deliveryPerformance', 'me')
     });
   }
 });
