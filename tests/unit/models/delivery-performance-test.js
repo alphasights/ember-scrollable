5bb05efd378127e96cf22c4343e8558027fa8fd2
Matteo Depalo <matteodepalo@gmail.com>
2015-02-23 17:28:19 +0000

    Add ember-cli-timecop

--- tests/unit/models/delivery-performance-test.js
+++ tests/unit/models/delivery-performance-test.js
@@ -10,9 +10,16 @@ moduleForModel('delivery-performance', 'DeliveryPerformance', {
   ],
 
   beforeEach: function() {
+    Timecop.install();
+    Timecop.freeze(new Date(2015, 2, 20, 0, 0));
+
     this.model = this.subject(
       { currentMonthCreditCount: 10, monthlyTarget: 30 }
     );
+  }
+
+  afterEach: function() {
+    Timecop.uninstall();
   }
 });
 
