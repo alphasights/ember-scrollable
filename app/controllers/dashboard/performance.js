d5c5365df4562c217f711a3b96fa930adb1c0bf5
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-23 10:58:50 -0500

    Move delivery performance functions to model

--- app/controllers/dashboard/performance.js
+++ app/controllers/dashboard/performance.js
@@ -1,55 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  hasFulfilledTarget: Ember.computed.gte('currentMonthCreditCount', 'monthlyTarget'),
-  isOnPace: Ember.computed.gte('currentMonthCreditCount', 'onPaceCreditTarget'),
-
+export default Ember.ObjectController.extend({
   performanceBarClass: function() {
-    if (this.get('hasFulfilledTarget')) {
+    if (this.get('model.hasFulfilledTarget')) {
       return 'gold-bar';
-    } else if (this.get('isOnPace')) {
+    } else if (this.get('model.isOnPace')) {
       return 'green-bar';
     } else {
       return 'red-bar';
     }
-  }.property('hasFulFilledTarget', 'isOnPace'),
-
-  onPaceCreditTarget: function() {
-    return Math.round(
-      (this.get('model.monthlyTarget') * this.monthCompletionProgress()
-    ) * 10) / 10;
-  }.property('model.monthlyTarget'),
-
-  weekDayHoursSinceBeginningOfMonth: function() {
-    var yesterdaysDate = moment().date() - 1;
-
-    return this.numberOfWeekdayHoursUntil(yesterdaysDate)
-      + this.hoursSinceTodaysStart();
-  },
-
-  totalWeekDayHoursInCurrentMonth: function() {
-    return this.numberOfWeekdayHoursUntil(moment().endOf('month').date());
-  },
-
-  monthCompletionProgress: function() {
-    return this.weekDayHoursSinceBeginningOfMonth()
-      / this.totalWeekDayHoursInCurrentMonth();
-  },
-
-  numberOfWeekdayHoursUntil: function(endDate) {
-    return _.range(1, endDate)
-      .map(function(day) { return moment().date(day); })
-      .reduce(function(memo, date) {
-        if (date.day() !== 6 || date.day() !== 0) {
-          return memo += 24;
-        }
-      }, 0);
-  },
-
-  hoursSinceTodaysStart: function() {
-    var todaysStart = moment().startOf('day');
-    var timeSinceTodaysStart = moment.duration(moment().diff(todaysStart));
-
-    return Math.round(timeSinceTodaysStart.asHours());
-  }
+  }.property('model.hasFulFilledTarget', 'model.isOnPace')
 });
--- app/models/delivery-performance.js
+++ app/models/delivery-performance.js
@@ -3,5 +3,46 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   currentMonthCreditCount: DS.attr('number'),
   monthlyTarget: DS.attr('number'),
-  user: DS.belongsTo('user')
+  user: DS.belongsTo('user'),
+  hasFulfilledTarget: Ember.computed.gte('currentMonthCreditCount', 'monthlyTarget'),
+  isOnPace: Ember.computed.gte('currentMonthCreditCount', 'onPaceCreditTarget'),
+
+  onPaceCreditTarget: function() {
+    return Math.round(
+      (this.get('monthlyTarget') * this.monthCompletionProgress()
+    ) * 10) / 10;
+  }.property('monthlyTarget'),
+
+  weekDayHoursSinceBeginningOfMonth: function() {
+    var yesterdaysDate = moment().date() - 1;
+
+    return this.numberOfWeekdayHoursUntil(yesterdaysDate)
+      + this.hoursSinceTodaysStart();
+  },
+
+  totalWeekDayHoursInCurrentMonth: function() {
+    return this.numberOfWeekdayHoursUntil(moment().endOf('month').date());
+  },
+
+  monthCompletionProgress: function() {
+    return this.weekDayHoursSinceBeginningOfMonth()
+      / this.totalWeekDayHoursInCurrentMonth();
+  },
+
+  numberOfWeekdayHoursUntil: function(endDate) {
+    return _.range(1, endDate)
+      .map(function(day) { return moment().date(day); })
+      .reduce(function(memo, date) {
+        if (date.day() !== 6 || date.day() !== 0) {
+          return memo += 24;
+        }
+      }, 0);
+  },
+
+  hoursSinceTodaysStart: function() {
+    var todaysStart = moment().startOf('day');
+    var timeSinceTodaysStart = moment.duration(moment().diff(todaysStart));
+
+    return Math.round(timeSinceTodaysStart.asHours());
+  }
 });
