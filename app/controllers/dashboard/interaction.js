037864cbb863eae30df972ada1a0b53f28e9aa16
Eugenio Depalo <eugeniodepalo@gmail.com>
2015-02-23 17:02:59 +0000

    WIP: Add header to interaction profile

--- app/controllers/dashboard/interaction.js
+++ app/controllers/dashboard/interaction.js
@@ -8,6 +8,29 @@ export default Ember.ObjectController.extend(ModelsNavigationMixin, {
   navigableModels: Ember.computed.oneWay('dashboard.upcomingInteractions'),
   modelRouteParams: ['dashboard.interaction'],
 
+  profiles: function() {
+    var advisor = this.get('advisor');
+    var clientContact = this.get('clientContact');
+
+    return [{
+      person: advisor,
+      id: 'advisor',
+      title: 'Advisor',
+      callTime: this.localTime(advisor)
+    }, {
+      person: clientContact,
+      id: 'client',
+      title: 'Client',
+      callTime: this.localTime(clientContact)
+    }];
+  }.property('advisor', 'clientContact', 'scheduledCallTime'),
+
+  localTime: function(person) {
+    return moment
+      .tz(this.get('scheduledCallTime'), person.get('timeZone'))
+      .format('h:mm A z');
+  },
+
   actions: {
     hideSidePanel: function() {
       this.transitionToRoute('dashboard');
