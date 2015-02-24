144fb6e24fdb8ec945fb22cb404724121409e80c
Matteo Depalo <matteodepalo@gmail.com>
2015-02-24 11:36:16 +0000

    Refactor delivery performance test descriptions

--- tests/unit/models/delivery-performance-test.js
+++ tests/unit/models/delivery-performance-test.js
@@ -20,7 +20,7 @@ moduleForModel('delivery-performance', 'DeliveryPerformance', {
   }
 });
 
-test('#onPaceCreditTarget', function() {
+test('#onPaceCreditTarget calculates the on pace credit target, rounded to the nearest tenth', function() {
   // Five weekdays prior to February 8 of a possible total of 20 weekdays.
   // Therefore 25% of the month has been completed.
   Timecop.freeze(new Date(2015, 1, 8, 0, 0));
@@ -29,84 +29,60 @@ test('#onPaceCreditTarget', function() {
     this.model.set('monthlyTarget', 30);
   });
 
-  equal(
-    this.model.get('onPaceCreditTarget'), 7.5,
-    'calculates the on pace credit target, rounded to the nearest tenth'
-  );
+  equal(this.model.get('onPaceCreditTarget'), 7.5);
 });
 
-test('#weekdayHoursSinceBeginningOfMonth', function() {
+test('#weekdayHoursSinceBeginningOfMonth calculates weekday hours including previous hours of the current day', function() {
   // February 1, 7, 8 were weekend days. leaving five full weekdays.
   // The current day, February 9, was a Monday and 12 hours had passed.
   Timecop.freeze(new Date(2015, 1, 9, 12, 0));
   var weekdayHours = (5 * 24) + 12;
 
-  equal(
-    this.model.weekdayHoursSinceBeginningOfMonth(), weekdayHours,
-    'correctly calculates weekday hours including previous hours of the current day'
-  );
+  equal(this.model.weekdayHoursSinceBeginningOfMonth(), weekdayHours);
 });
 
-test('#totalWeekdayHoursInCurrentMonth', function() {
+test('#totalWeekdayHoursInCurrentMonth calculates the number of weekday hours in the current month', function() {
   // There are 20 weekdays in the month of February.
   // http://www.wolframalpha.com/input/?i=number+of+weekdays+in+February+2015
   Timecop.freeze(new Date(2015, 1, 20, 12, 0));
   var weekdayHoursInMonth = 20 * 24;
 
-  equal(
-    this.model.totalWeekdayHoursInCurrentMonth(), weekdayHoursInMonth,
-    'correctly calculates the number of weekday hours in the current month'
-  );
+  equal(this.model.totalWeekdayHoursInCurrentMonth(), weekdayHoursInMonth);
 });
 
-test('#monthCompletionProgress', function() {
+test('#monthCompletionProgress calculates how far along the month has completed', function() {
   // Five weekdays prior to February 8 of a possible total of 20 weekdays.
   Timecop.freeze(new Date(2015, 1, 8, 0, 0));
-  var monthProgress = 120 / 480;
+  var monthProgress = (5 * 24) / (20 * 24);
 
-  equal(
-    this.model.monthCompletionProgress(), monthProgress,
-    'correctly calculates how far along the month has completed'
-  );
+  equal(this.model.monthCompletionProgress(), monthProgress);
 });
 
-test('#numberOfWeekdayHoursUntil', function() {
+test('#numberOfWeekdayHoursUntil calculates weekday hours by excluding weekends', function() {
   // February 1, 7, 8 were weekend days, leaving five weekdays before February 8.
   Timecop.freeze(new Date(2015, 1, 8, 0, 0));
   var totalHours = 5 * 24;
 
-  equal(
-    this.model.numberOfWeekdayHoursUntil(8), totalHours,
-    'correctly calculates weekday hours by excluding weekends'
-  );
+  equal(this.model.numberOfWeekdayHoursUntil(8), totalHours);
 });
 
-test('#hoursSinceTodaysStart', function() {
+test("#hoursSinceTodaysStart calculates the number of hours since the day's start", function() {
   // February 20, 2015 12pm
   Timecop.freeze(new Date(2015, 1, 20, 12, 0));
 
-  equal(
-    this.model.hoursSinceTodaysStart(), 12,
-    "correctly calculates the number of hours since the day's start"
-  );
+  equal(this.model.hoursSinceTodaysStart(), 12);
 });
 
-test('#hoursSinceTodaysStart', function() {
+test("#hoursSinceTodaysStart handles rounding down the number of hours since the day's start", function() {
   // February 20, 2015 12:29pm
   Timecop.freeze(new Date(2015, 1, 20, 12, 29));
 
-  equal(
-    this.model.hoursSinceTodaysStart(), 12,
-    "correctly handles rounding down the number of hours since the day's start"
-  );
+  equal(this.model.hoursSinceTodaysStart(), 12);
 });
 
-test('#hoursSinceTodaysStart', function() {
+test("#hoursSinceTodaysStart handles rounding up the number of hours since the day's start", function() {
   // February 20, 2015 11:30pm
   Timecop.freeze(new Date(2015, 1, 20, 11, 30));
 
-  equal(
-    this.model.hoursSinceTodaysStart(), 12,
-    "correctly handles rounding up the number of hours since the day's start"
-  );
+  equal(this.model.hoursSinceTodaysStart(), 12);
 });
