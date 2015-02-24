dbb35dbb8e4d65d9126f1e2ff7a410e8430636d6
Andrei Popa <andrei.deioo@gmail.com>
2015-02-24 17:41:22 +0000

    Show all performance tooltip when hovering the bar

--- app/views/dashboard/widgets/performance.js
+++ app/views/dashboard/widgets/performance.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  classNameBindings: [':widget', ':performance', 'controller.performanceBarClass']
+  classNameBindings: [':widget', ':performance', 'controller.performanceBarClass'],
+
+  onDidInsertElement: function() {
+    this.$().hover(() => {
+      this.$('.tooltipstered').tooltipster('show');
+    }, () => {
+      this.$('.tooltipstered').tooltipster('hide');
+    });
+  }.on('didInsertElement')
 });
