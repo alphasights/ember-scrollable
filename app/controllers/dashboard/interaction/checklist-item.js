31e6f4becaa605b697a74686f8f2363177477f7f
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-02-25 15:33:58 -0500

    Include description for Client Oversight Question checklist item

--- app/controllers/dashboard/interaction/checklist-item.js
+++ app/controllers/dashboard/interaction/checklist-item.js
@@ -9,7 +9,8 @@ const typeToDescriptionMap = {
   internalProfileCheck: "Internal Profile Check",
   bainInteractionConfirmation: "Send Bain confirmation email",
   careerHistoryConfirmation: "Obtain Advisor's career history approval",
-  careerHistoryNotification: "Notify client compliance the career history has changed"
+  careerHistoryNotification: "Notify client compliance the career history has changed",
+  clientOversightQuestion: "Client Oversight Question"
 };
 
 export default Ember.ObjectController.extend({
