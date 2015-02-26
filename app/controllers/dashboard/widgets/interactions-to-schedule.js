8dea3721ac740d9af058d7e1da7049c69c532bdf
Matteo Depalo <matteodepalo@gmail.com>
2015-02-26 15:12:18 +0000

    showMore -> isShowingAll

--- app/controllers/dashboard/widgets/interactions-to-schedule.js
+++ app/controllers/dashboard/widgets/interactions-to-schedule.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  showMore: false,
+  isShowingAll: false,
   initialVisibleLimit: 4,
 
   visibleInteractions: function() {
-    if (this.get('showMore')) {
+    if (this.get('isShowingAll')) {
       return this.get('model');
     } else {
       return this.get('model').slice(0, this.get('initialVisibleLimit'));
     }
-  }.property('model', 'showMore'),
+  }.property('model', 'isShowingAll'),
 
   canShowMoreInteractions: function() {
     return this.get('visibleInteractions.length') < this.get('model.length');
@@ -18,11 +18,11 @@ export default Ember.ArrayController.extend({
 
   actions: {
     showMore: function() {
-      this.set('showMore', true);
+      this.set('isShowingAll', true);
     },
 
     showLess: function() {
-      this.set('showMore', false);
+      this.set('isShowingAll', false);
     }
   }
 });
