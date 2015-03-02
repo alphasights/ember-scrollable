d7107feef0bfe47d22b48ca01cc1a150d907057c
Matteo Depalo <matteodepalo@gmail.com>
2015-03-02 12:47:03 +0000

    Refactor interactions widget title

--- app/views/dashboard/widget.js
+++ app/views/dashboard/widget.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   classNameBindings: [':widget'],
-  layoutName: 'dashboard/widget'
+  layoutName: 'dashboard/widget',
+  title: Ember.computed.oneWay('name')
 });
--- app/views/dashboard/widgets/interactions.js
+++ app/views/dashboard/widgets/interactions.js
@@ -6,5 +6,13 @@ export default WidgetView.extend({
   classNameBindings: [':interactions', 'isCollapsed:collapsed'],
 
   isCollapsed: Ember.computed.oneWay('controller.isCollapsed'),
-  listItemTemplateName: null
+  listItemTemplateName: null,
+
+  title: function() {
+    if (Ember.isPresent(this.get('controller.visibleContent'))) {
+      return `${this.get('name')} (${this.get('controller.visibleContent.length')} of ${this.get('controller.length')})`;
+    } else {
+      return this.get('name');
+    }
+  }.property('name', 'visibleContent.length', 'length')
 });
