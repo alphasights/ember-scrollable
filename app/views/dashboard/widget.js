504f68092e7d82c2ff440bc99b49a9b0e9fe4579
Matteo Depalo <matteodepalo@gmail.com>
2015-03-02 14:14:08 +0000

    Add name default value

--- app/views/dashboard/widget.js
+++ app/views/dashboard/widget.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.View.extend({
   classNameBindings: [':widget'],
   layoutName: 'dashboard/widget',
+  name: null,
   title: Ember.computed.oneWay('name')
 });
