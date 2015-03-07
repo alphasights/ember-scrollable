53835bdec0d4687e1b1a5d2779ad86bd348d379d
Eugenio Depalo <eugeniodepalo@gmail.com>
2015-03-07 13:01:30 +0000

    occurrences -> unavailabilities

--- app/controllers/dashboard/schedule-interaction.js
+++ app/controllers/dashboard/schedule-interaction.js
@@ -8,11 +8,17 @@ export default Ember.ObjectController.extend(ModelsNavigationMixin, {
   navigableModels: Ember.computed.oneWay('dashboard.interactionsToSchedule'),
   modelRouteParams: ['dashboard.schedule-interaction'],
 
-  occurrences: [
+  unavailabilities: [
     Ember.Object.create({
       type: 'alpha-call',
       time: moment().startOf('week').add(10, 'hour'),
       duration: moment.duration(5, 'hour')
+    }),
+
+    Ember.Object.create({
+      type: 'alpha-call',
+      time: moment().startOf('week').add(8, 'hour').add(3, 'day'),
+      duration: moment.duration(2, 'hour')
     })
   ],
 
