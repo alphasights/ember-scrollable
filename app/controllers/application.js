1141f6acf04b3a04ee40fd628698eb944512e236
Matteo Depalo <matteodepalo@gmail.com>
2015-03-13 16:18:40 +0000

    Abstract navigation items

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['currentUser'],
-  currentUser: Ember.computed.oneWay('controllers.currentUser')
+  currentUser: Ember.computed.oneWay('controllers.currentUser'),
+
+  navigationItems: [{
+    id: 'dashboard', name: 'Dashboard', routeName: 'dashboard'
+  }, {
+    id: 'projects', name: 'Projects', routeName: 'projects'
+  }, {
+    id: 'team', name: 'Teams', routeName: 'teams'
+  }, {
+    id: 'performance', name: 'Performance', routeName: 'performance'
+  }]
 });
