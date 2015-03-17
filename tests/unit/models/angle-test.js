4a0d6c06f185d6285b00e60ea534edc51392c8bd
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-03-17 12:10:17 -0400

    Remove observer tests on the Angle

--- tests/unit/models/angle-test.js
+++ tests/unit/models/angle-test.js
@@ -31,7 +31,9 @@ test("#memberships returns the angleTeamMemberships", function(assert) {
   var angleTeamMembership;
 
   Ember.run(() => {
-    angleTeamMembership = this.store().push('angleTeamMembership', { id: 3, angle: 1 });
+    angleTeamMembership = this.store().push(
+      'angleTeamMembership', { id: 3, angle: 1 }
+    );
   });
 
   this.model.get('memberships').forEach((membership) => {
@@ -45,44 +47,12 @@ test("#members returns the users connected via angle team membership", function(
 
   Ember.run(() => {
     user = this.store().push('user', { id: 2, angles: [1] });
-    angleTeamMembership = this.store().push('angleTeamMembership', { id: 3, angle: 1, teamMember: 2 });
+    angleTeamMembership = this.store().push(
+      'angleTeamMembership', { id: 3, angle: 1, teamMember: 2 }
+    );
   });
 
   this.model.get('members').forEach((member) => {
     assert.ok(member === user);
   }, 'contains the correct user in the members array');
 });
-
-
-// test("changing memberships updates the membershipsUpdateAt timestamp", function(assert) {
-//   assert.equal(this.model.get('membershipsUpdatedAt'), null);
-//
-//   var timecopTime = moment('2015-02-09T12:00:00.000+00:00');
-//   Timecop.freeze(timecopTime);
-//
-//   Ember.run(() => {
-//     var angleTeamMembership = this.store().createRecord('angleTeamMembership');
-//     this.model.get('angleTeamMemberships').pushObject(angleTeamMembership);
-//   });
-//
-//   assert.equal(
-//     this.model.get('membershipsUpdatedAt'), timecopTime.toDate().toTimeString()
-//   );
-// });
-//
-// test("changing members updates the membersUpdateAt timestamp", function(assert) {
-//   assert.equal(this.model.get('membersUpdatedAt'), null);
-//
-//   var timecopTime = moment('2015-02-09T12:00:00.000+00:00');
-//   Timecop.freeze(timecopTime);
-//
-//   Ember.run(() => {
-//     var user = this.store().createRecord('user');
-//     var angleTeamMembership = this.store().createRecord('angleTeamMembership');
-//     angleTeamMembership.setProperties({angle: this.model, teamMember: user});
-//   });
-//
-//   assert.equal(
-//     this.model.get('membersUpdatedAt'), timecopTime.toDate().toTimeString()
-//   );
-// });
