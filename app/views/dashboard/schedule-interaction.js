01bb944085c70ad6528f7be354265c4d289013bf
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-03-19 14:36:25 -0400

    Animate closing of side panel when cancelling interaction

--- app/views/dashboard/schedule-interaction.js
+++ app/views/dashboard/schedule-interaction.js
@@ -1,5 +1,12 @@
 import InteractionView from 'phoenix/views/dashboard/interaction';
 
 export default InteractionView.extend({
-  classNameBindings: [':schedule-interaction']
+  classNameBindings: [':schedule-interaction'],
+
+  actions: {
+    cancel: function() {
+      this.get('controller').send('cancel');
+      this.send('close');
+    }
+  }
 });
