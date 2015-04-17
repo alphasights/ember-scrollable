50f2a24c5e4fe81f4211bd2bce7b6d2b575db527
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-04-17 09:56:31 -0400

    Store interaction id in Unavailability

--- app/models/unavailability.js
+++ app/models/unavailability.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   startsAt: DS.attr('date'),
   endsAt: DS.attr('date'),
-  projectId: DS.attr('number'),
-  advisorId: DS.attr('number')
+  interactionId: DS.attr('number')
 });
