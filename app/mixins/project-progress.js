e2e0481ed6a79154a37cac80b3b33a3b3b664600
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-05-01 10:17:34 -0400

    Remove deprecation warnings from Project Progress Mixin

--- app/mixins/project-progress.js
+++ app/mixins/project-progress.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  progress: Ember.computed('deliveredAdvisorsCount', 'deliveryTarget', function() {
-    var deliveryTarget = this.get('deliveryTarget');
+  progress: Ember.computed('model.deliveredAdvisorsCount', 'model.deliveryTarget', function() {
+    var deliveryTarget = this.get('model.deliveryTarget');
 
     if (deliveryTarget === 0) {
       return 0;
     } else {
-      return this.get('deliveredAdvisorsCount') / deliveryTarget;
+      return this.get('model.deliveredAdvisorsCount') / deliveryTarget;
     }
   })
 });
