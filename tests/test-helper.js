3d793221f7484545629e43616e908ebc19232ce0
Eugenio Depalo <eugenio@alphasights.com>
2015-05-05 10:32:36 +0100

    Fix test fixtures

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -19,6 +19,7 @@ export default {
     this.app.fixtures.define('GET', '/users/me', {
       response: {
         "user": {
+          "name": "Example User",
           "initials": "EU",
           "id": 1,
           "teamId": 1,
@@ -39,6 +40,18 @@ export default {
 
     this.app.fixtures.define('GET', '/interactions', { response: {
       "interactions": []
+    }});
+
+    this.app.fixtures.define('GET', '/teams', { response: {
+      "teams": [{
+        "name": "Example Team",
+        "id": 1,
+        "office": "Example Office"
+      }, {
+        "name": "Example Team 2",
+        "id": 2,
+        "office": "Example Office"
+      }]
     }});
   },
 
