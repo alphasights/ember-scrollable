6721e3077265ccdd56caa3786238911b1dcdd082
Andrei Popa <andrei.deioo@gmail.com>
2015-05-14 12:21:59 +0100

    Implement flip panel for the advisor / client contact panels

--- app/components/as-profile.js
+++ app/components/as-profile.js
@@ -1,9 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNameBindings: [':profile'],
+  classNameBindings: [':profile', 'isFlipped:flipped'],
   tagName: 'article',
-  
+
   person: null,
-  title: null
+  title: null,
+
+  mainPhoneNumber: function() {
+    return this.get('person.phoneNumbers').get('firstObject');
+  }.property('person.phoneNumbers'),
+
+  isFlipped: function() {
+      return this.get('flipped');
+  }.property('flipped'),
+
+  actions: {
+     flip: function() {
+        this.toggleProperty('flipped');
+     }
+  }
 });
