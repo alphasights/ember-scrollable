5805dd31b107fe16896e6b017974448a4b5dbd03
Eugenio Depalo <eugenio@alphasights.com>
2015-05-14 10:09:47 +0100

    Include jquery simulate only in tests

--- Brocfile.js
+++ Brocfile.js
@@ -38,8 +38,11 @@ app.import('bower_components/underscore/underscore.js');
 app.import('bower_components/messenger/build/js/messenger.js');
 app.import('bower_components/messenger/build/css/messenger.css');
 app.import('bower_components/messenger/build/css/messenger-theme-block.css');
-app.import('bower_components/jquery-simulate-ext/libs/jquery.simulate.js');
-app.import('bower_components/jquery-simulate-ext/src/jquery.simulate.ext.js');
-app.import('bower_components/jquery-simulate-ext/src/jquery.simulate.drag-n-drop.js');
+
+if (EmberApp.env() === 'test') {
+  app.import('bower_components/jquery-simulate-ext/libs/jquery.simulate.js', { type: 'test' });
+  app.import('bower_components/jquery-simulate-ext/src/jquery.simulate.ext.js', { type: 'test' });
+  app.import('bower_components/jquery-simulate-ext/src/jquery.simulate.drag-n-drop.js', { type: 'test' });
+}
 
 module.exports = app.toTree();
