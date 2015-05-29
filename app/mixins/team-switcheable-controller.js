27e798093eb5d2e81e3cb892a6de6305705ef1c6
Matteo Depalo <matteodepalo@gmail.com>
2015-05-29 08:48:31 -0400

    Refactor team switcher into a component

--- app/mixins/team-switcheable-controller.js
+++ app/mixins/team-switcheable-controller.js
@@ -6,25 +6,13 @@ export default Ember.Mixin.create({
   teamId: null,
   teams: Ember.computed.oneWay('currentUser.teams'),
 
-  selectedTeam: Ember.computed('teamId', 'teams.@each.id', function(_, value) {
-    if (arguments.length > 1) {
-      if (value != null) {
-        this.set('teamId', value.get('id'));
-      } else {
-        this.set('teamId', null);
-      }
-    }
-
-    var teamId = this.get('teamId');
-
-    if (teamId != null) {
-      return this.get('teams').findBy('id', teamId);
-    } else {
-      return null;
-    }
-  }),
-
   queryParams: {
     teamId: 'team_id'
   },
+
+  actions: {
+    selectTeam: function(teamId) {
+      this.set('teamId', teamId);
+    }
+  }
 });
