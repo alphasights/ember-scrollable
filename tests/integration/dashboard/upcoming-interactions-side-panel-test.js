5a582d90a3aa3261a5a9877cb3392da5f0a74ae1
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-06-01 14:29:29 -0400

    Add test for cancelling scheduling interactions from side panel

---
+++ tests/integration/dashboard/upcoming-interactions-side-panel-test.js
@@ -0,0 +1,208 @@
+import Ember from 'ember';
+import { test } from 'ember-qunit';
+import '../../helpers/define-fixture';
+import testHelper from '../../test-helper';
+
+const interaction = {
+  id: 1
+};
+
+QUnit.module("Upcoming Interactions Side Panel", {
+  beforeEach: function() {
+    testHelper.beforeEach.apply(this, arguments);
+
+    defineFixture('GET', '/interactions', { params: { primary_contact_id: "1" }, response: {
+      "advisors": [
+        {
+          "id": 1,
+          "avatar_url": null,
+          "emails": ['advisor@email.com'],
+          "name": 'Johnny Advisor',
+          "phone_numbers": ['+1 555-123-4567'],
+          "phones": [
+            {
+              "number": '+1 555-123-4567',
+              "type": 'mobile',
+              "primary": true
+            }
+          ],
+          "job_title": 'Vice President',
+          "company_name": 'Apple'
+        }
+      ],
+      "client_contacts": [
+        {
+          "id": 21387,
+          "avatar_url": null,
+          "emails": ['client@email.com'],
+          "name": 'Bob Client',
+          "phone_numbers": ['+1 555-321-9000'],
+          "phones": [
+            {
+              "number": '+1 555-321-9000',
+              "type": 'mobile',
+              "primary": true
+            }
+          ],
+          "client_account_id": 485
+        }
+      ],
+      "client_accounts": [
+        {
+           "id": 485,
+           "name": 'McKinsey & Company San Francisco'
+        }
+      ],
+      "projects": [
+        {
+           "id": 32522,
+           "status": "high",
+           "name": 'Project Name',
+           "client_code": "MCKU",
+           "details_url": "/projects/32522",
+           "index": 3,
+           "created_at": "2015-01-23T21:01:33.615+00:00",
+           "angle_ids": [40380],
+           "analyst_1_id": 6565389
+        }
+      ],
+      "interactions": [
+        {
+          "id": interaction.id,
+          "scheduled_call_time": '2015-02-18T10:00:00.000+00:00',
+          "advisor_id": 1,
+          "client_contact_id": 21387,
+          "project_id": 32522,
+          "checklist_item_ids": [],
+          "requested_at": '2015-02-18T10:00:00.000+00:00',
+          "actioned": false
+        }
+      ],
+      "checklist_items": []
+    }});
+  },
+
+  afterEach: function() {
+    testHelper.afterEach.apply(this, arguments);
+  }
+});
+
+test("Cancel interaction returns to dashboard and removes interaction from the widget", function(assert) {
+  defineFixture('DELETE', '/interests/1', { params: { "withdraw_from_compliance": "false" }, response: {
+    "interactions": [
+      {
+        "id": 1,
+        "scheduled_call_time": null,
+        "actioned": null,
+        "requested_at": null,
+        "checklist_item_ids": [],
+        "interaction_type": 'One-on-one Call',
+        "advisor_time_zone": "PST",
+        "advisor_phone_country_code": "+1",
+        "advisor_phone_number": "7654321",
+        "client_time_zone": "EST",
+        "dial_in_number": "1234567",
+        "primary_contact_id": 1,
+        "speak": true,
+        "client_access_number_country": "US",
+      }
+    ]
+  }});
+
+  visit('/dashboard');
+
+  andThen(function() {
+    assert.equal(find('.upcoming-interactions article').length, 1,
+    'shows the upcoming interaction in the widget');
+  });
+
+  click('.upcoming-interactions article:first');
+  click("a:contains('Cancel Interaction')");
+  click("button:contains('Yes, Cancel Interaction')");
+
+  andThen(function() {
+    assert.equal(currentURL(), '/dashboard',
+      'returns the user to dashboard after cancelling'
+    );
+
+    assert.equal(find('.upcoming-interactions article').length, 0,
+      'removes the interaction from the widget'
+    );
+
+    var message = $('.messenger .messenger-message-inner').first().text().trim();
+    assert.equal(message, "The interaction has been cancelled.");
+  });
+});
+
+test("Cancel and withdraw interaction returns to dashboard and removes interaction from the widget", function(assert) {
+  defineFixture('DELETE', '/interests/1', { params: { "withdraw_from_compliance": "true" }, response: {
+    "interactions": [
+      {
+        "id": 1,
+        "scheduled_call_time": null,
+        "actioned": null,
+        "requested_at": null,
+        "checklist_item_ids": [],
+        "interaction_type": 'One-on-one Call',
+        "advisor_time_zone": "PST",
+        "advisor_phone_country_code": "+1",
+        "advisor_phone_number": "7654321",
+        "client_time_zone": "EST",
+        "dial_in_number": "1234567",
+        "primary_contact_id": 1,
+        "speak": true,
+        "client_access_number_country": "US",
+      }
+    ]
+  }});
+
+  visit('/dashboard');
+
+  andThen(function() {
+    assert.equal(find('.upcoming-interactions article').length, 1,
+    'shows the upcoming interaction in the widget');
+  });
+
+  click('.upcoming-interactions article:first');
+  click("a:contains('Cancel and Withdraw Interaction')");
+  click("button:contains('Yes, Cancel and Withd')");
+
+  andThen(function() {
+    assert.equal(currentURL(), '/dashboard',
+      'returns the user to dashboard after cancelling'
+    );
+
+    assert.equal(find('.upcoming-interactions article').length, 0,
+      'removes the interaction from the widget'
+    );
+
+    var message = $('.messenger .messenger-message-inner').first().text().trim();
+    assert.equal(message, "The interaction has been cancelled.");
+  });
+});
+
+test("Cancel Interaction Failure", function(assert) {
+  defineFixture('DELETE', '/interests/1', { status: 500 });
+
+  visit('/dashboard');
+
+  andThen(function() {
+    assert.equal(find('.upcoming-interactions article').length, 1,
+    'shows the original scheduled interaction in the widget');
+  });
+
+  click('.upcoming-interactions article:first');
+  click("a:contains('Cancel Interaction')");
+  click("button:contains('Yes, Cancel Interaction')");
+
+  andThen(function() {
+    assert.equal(find('.upcoming-interactions article').length, 1,
+      'does not remove the interaction from the widget'
+    );
+
+    var message = $('.messenger .messenger-message-inner').first().text().trim();
+    assert.equal(message, "The interaction could not be cancelled.",
+      'displays a message that it could not be cancelled'
+    );
+  });
+});
