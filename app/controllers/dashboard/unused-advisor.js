e20db77336d85a7aee0a7668097393671ee23b62
Matteo Depalo <matteodepalo@gmail.com>
2015-06-02 12:06:58 -0400

    Implement unused advisor sidepanel

--- app/controllers/dashboard/unused-advisor.js
+++ app/controllers/dashboard/unused-advisor.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ModelsNavigationMixin from 'ember-cli-paint/mixins/models-navigation';
+import notify from 'phoenix/helpers/notify';
 
 export default Ember.Controller.extend(ModelsNavigationMixin, {
   needs: ['dashboard'],
@@ -11,6 +12,19 @@ export default Ember.Controller.extend(ModelsNavigationMixin, {
   actions: {
     hideSidePanel: function() {
       this.transitionToRoute('dashboard');
+    },
+
+    remove: function() {
+      if (window.confirm('Are you sure you want to remove the advisor from the list?')) {
+        this.get('model').destroyRecord().then(() => {
+          notify(`The advisor ${this.get('model.advisor.name')} was removed from the list`);
+          this.get('sidePanel').send('close');
+        });
+      }
+    },
+
+    followUp: function() {
+      // open the double sidepanel
     }
   }
 });
--- app/routes/dashboard/unused-advisor.js
+++ app/routes/dashboard/unused-advisor.js
@@ -3,6 +3,6 @@ import SidePanelRouteMixin from 'ember-cli-paint/mixins/side-panel-route';
 
 export default Ember.Route.extend(SidePanelRouteMixin, {
   model: function(params) {
-    return this.store.find('unusedAdvisor', params.advisor_id);
+    return this.store.find('unusedAdvisor', params.unused_advisor_id);
   }
 });
