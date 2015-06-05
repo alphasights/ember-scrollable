69b884e9fb3d934eb75bc8bf5c9b5d059b6fef04
Ju Liu <ju.liu@welaika.com>
2015-06-05 09:49:08 +0100

    Remove user with id=me from the store

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -10,6 +10,7 @@ export default Ember.Service.extend({
 
   authenticate: function() {
     return this.store.find('user', 'me').then((user) => {
+      this.store.recordForId('user', 'me').unloadRecord();
       this.set('model', user);
       return user;
     });
