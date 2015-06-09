600253a3611afeb244ade86fd7cc5da401b51259
Eugenio Depalo <eugenio@alphasights.com>
2015-06-09 18:08:59 +0100

    Add used dependency to interactionsToSchedule

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -37,14 +37,16 @@ export default Ember.Controller.extend(TeamSwitcheableControllerMixin, {
       .sortBy('scheduledCallTime');
   }),
 
-  interactionsToSchedule: Ember.computed('model.interactions.[]', function() {
-    return this.get('model.interactions').filter(function(interaction) {
-      return interaction.get('requestedAt') != null &&
-        interaction.get('scheduledCallTime') == null &&
-        !interaction.get('actioned');
-    }).sort(function(a, b) {
-      return -Ember.compare(a.get('requestedAt'), b.get('requestedAt'));
-    });
+  interactionsToSchedule: Ember.computed('model.interactions.@each.used', function() {
+    return this.get('model.interactions')
+      .filterBy('used', false)
+      .filter(function(interaction) {
+        return interaction.get('requestedAt') != null &&
+          interaction.get('scheduledCallTime') == null &&
+          !interaction.get('actioned');
+      }).sort(function(a, b) {
+        return -Ember.compare(a.get('requestedAt'), b.get('requestedAt'));
+      });
   }),
 
   unusedAdvisors: Ember.computed('model.unusedAdvisors.[]', function() {
