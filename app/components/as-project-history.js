952d1912c92257835ab6f8ee2ec70397ad3db717
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-06-11 14:33:28 -0400

    Extract common expandable list styles

--- app/components/as-project-history.js
+++ app/components/as-project-history.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNameBindings: [':project-history'],
 
-  projectHistory: null
+  projectHistory: null,
+
+  actions: {
+    openProject: function(project) {
+      window.open(project.get('pistachioUrl'));
+    }
+  }
 });
