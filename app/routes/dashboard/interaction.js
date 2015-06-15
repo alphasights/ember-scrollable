3b4ae73870b169a3de3a0d13938cc73238c55d6f
Matteo Depalo <matteodepalo@gmail.com>
2015-06-15 10:43:49 -0400

    Fix titleToken on the interaction page

--- app/routes/dashboard/interaction.js
+++ app/routes/dashboard/interaction.js
@@ -3,7 +3,7 @@ import SidePanelRouteMixin from 'ember-cli-paint/mixins/side-panel-route';
 
 export default Ember.Route.extend(SidePanelRouteMixin, {
   titleToken: function(model) {
-    let advisorName = model.get('advisor.name');
+    let advisorName = models.interaction.get('advisor.name');
 
     return `Interaction: ${advisorName}`;
   },
