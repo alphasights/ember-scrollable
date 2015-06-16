22c3a1f0d86e4a2e764f12fad4ab412b7b0ccf37
Matteo Depalo <matteodepalo@gmail.com>
2015-06-16 14:35:48 -0400

    Implement option groups in as-form/select component

--- app/components/as-form/select.js
+++ app/components/as-form/select.js
@@ -2,6 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   errorProperty: Ember.computed.oneWay('name'),
-  optionLabelPath: 'content.name',
-  optionValuePath: 'content.id'
+
+  groupedContent: Ember.computed('content.[]', 'optionGroupPath', function() {
+    var groupPath = this.get('optionGroupPath');
+    var groupedContent = [];
+    var content = this.get('content') || [];
+
+    content.forEach(function(item) {
+      var option = Ember.Object.create(item);
+      var label = option.get(groupPath);
+
+      if (groupedContent.get('lastObject.label') !== label) {
+        groupedContent.pushObject({
+          label: label,
+          content: []
+        });
+      }
+
+      groupedContent.get('lastObject.content').push(option);
+    });
+
+    return groupedContent;
+  })
 });
