ee25489d1dcc8f1f8affb530ac6a5545e3d21318
Matteo Depalo <matteodepalo@gmail.com>
2015-06-18 11:28:13 -0400

    Update ember data and fix deprecations

--- app/serializers/interaction.js
+++ app/serializers/interaction.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import { ActiveModelSerializer } from 'active-model-adapter';
 
-export default DS.ActiveModelSerializer.extend({
+export default ActiveModelSerializer.extend({
   serializeBelongsTo: function(snapshot, json, relationship) {
     if (!['primaryContact'].contains(relationship.key)) {
       this._super(snapshot, json, relationship);
