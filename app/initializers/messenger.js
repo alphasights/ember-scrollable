a26009d9286bf8c8f74744da91836e1128a93869
Andrei Popa <andrei.deioo@gmail.com>
2015-06-24 14:27:26 +0100

    Remove unused class

--- app/initializers/messenger.js
+++ app/initializers/messenger.js
@@ -4,7 +4,7 @@ export default {
   initialize: function() {
     Messenger.options = {
       theme: null,
-      extraClasses: 'notifications fixed'
+      extraClasses: 'notifications'
     };
   }
 };
