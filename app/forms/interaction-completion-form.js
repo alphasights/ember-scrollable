9bddab94e2088b46aa542a8436c7da4800c96107
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-06-25 14:32:24 -0400

    Add speak quality select field to completion form

--- app/forms/interaction-completion-form.js
+++ app/forms/interaction-completion-form.js
@@ -9,6 +9,19 @@ const qualityOptionsMapping = {
 
 const qualityOptions = ['good', 'bad'];
 
+const speakQualityOptionsMapping = {
+  'no_known_issues': 'No known issues',
+  'disconnected_10': 'Call disconnected: ≤10 mins',
+  'disconnected_30': 'Call disconnected: 11 - 30 mins',
+  'disconnected_60': 'Call disconnected: 31 - 60 mins',
+  'poor_line_quality': 'Poor line quality identified by client or advisor',
+  'breaking_up': 'Call broke up (jitter)',
+  'lag': 'Voice lag or delay',
+  'email_notification': 'Issues with email notifications',
+  'advisor_not_dialed': 'Advisor not dialed',
+  'other': 'Other issue'
+};
+
 export default Ember.ObjectProxy.extend(EmberValidations.Mixin, {
   requestPromise: null,
 
@@ -37,6 +50,15 @@ export default Ember.ObjectProxy.extend(EmberValidations.Mixin, {
       return Ember.Object.create({
         id: option,
         name: qualityOptionsMapping[option]
+      });
+    });
+  }),
+
+  speakQualityOptions: Ember.computed(function() {
+    return _.map(speakQualityOptionsMapping, function(value, key) {
+      return Ember.Object.create({
+        id: key,
+        name: value
       });
     });
   }),
