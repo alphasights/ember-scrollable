2574e68909782e23145e22f1adc8613b78c491d2
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-06-26 15:04:04 -0400

    High selected email in email correspondence

--- app/components/as-email-correspondence.js
+++ app/components/as-email-correspondence.js
@@ -5,9 +5,18 @@ export default Ember.Component.extend({
 
   emails: null,
   actionHandler: null,
+  selectedEmail: null,
 
   actions: {
     viewEmail: function(email) {
+      let previouslySelectedEmail = this.get('selectedEmail');
+
+      if (previouslySelectedEmail) {
+        previouslySelectedEmail.set('cssClass', '');
+      }
+
+      this.set('selectedEmail', email);
+      email.set('cssClass', 'selected');
       this.get('actionHandler').send('viewEmail', email);
     }
   }
