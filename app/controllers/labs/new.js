f223861af581cd3273292444cc4a4f07cbc951de
John Kelly Ferguson <hello@johnkellyferguson.com>
2015-07-14 16:31:26 -0400

    Transition to index after creating new feature

--- app/controllers/labs/new.js
+++ app/controllers/labs/new.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
+import notify from 'phoenix/helpers/notify';
 
 export default Ember.Controller.extend({
   actions: {
     createFeature: function() {
-      this.get('featureForm').save();
+      this.get('featureForm').save().then((featureForm) => {
+        let featureName = featureForm.get('name');
+
+        notify(`The feature, ${featureName}, has been created.`);
+        this.transitionToRoute('labs');
+      });
     }
   }
 });
