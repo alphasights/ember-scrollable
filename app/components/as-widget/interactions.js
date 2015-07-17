3fa8930da01821fd450ac3a107e5c3ba703503a1
Matteo Depalo <matteodepalo@gmail.com>
2015-07-17 15:23:23 -0400

    Update language

--- app/components/as-widget/interactions.js
+++ app/components/as-widget/interactions.js
@@ -3,14 +3,14 @@ import ListWidgetComponent from 'phoenix/components/as-widget/list';
 
 export default ListWidgetComponent.extend({
   classNameBindings: [':interactions'],
-  
+
   interactionsWithIncompleteChecklistItems: Ember.computed.filterBy('arrangedContent', 'hasIncompleteChecklistItems', true),
 
   alert: Ember.computed('interactionsWithIncompleteChecklistItems.length', function() {
     let count = this.get('interactionsWithIncompleteChecklistItems.length');
 
     if (count > 0) {
-      return `${count} ${count > 1 ? 'interactions' : 'interaction'} with an incomplete checklist in total`;
+      return `${count} ${count > 1 ? 'interactions have' : 'interaction has'} incomplete checklist items`;
     } else {
       return '';
     }
