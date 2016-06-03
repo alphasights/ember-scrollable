1cdd608ec29b3e7943a05cd0cd81d928e0196f80
Taras Mankovski <tarasm@gmail.com>
2016-06-03 18:45:20 -0400

    Pulling dependencies from node_modules

--- index.js
+++ index.js
@@ -2,22 +2,15 @@
 
 'use strict';
 
-var path = require('path');
-
 module.exports = {
   name: 'ember-scrollable',
 
-  included: function(app) {
-    this._super.included(app);
-
-    app.import(
-      path.join(app.bowerDirectory, 'trackpad-scroll-emulator/jquery.trackpad-scroll-emulator.js'),
-      { type: 'vendor' }
-    );
-
-    app.import(
-      path.join(app.bowerDirectory, 'trackpad-scroll-emulator/css/trackpad-scroll-emulator.css'),
-      { type: 'vendor' }
-    );
+  options: {
+    nodeAssets: {
+      "trackpad-scroll-emulator": {
+        srcDir: '/',
+        import: ['jquery.trackpad-scroll-emulator.js', 'css/trackpad-scroll-emulator.css']
+      }
+    }
   }
 };
