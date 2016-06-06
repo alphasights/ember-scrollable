d57694c5f63cc2aa1cec9931f28dd6499efa2a13
Taras Mankovski <tarasm@gmail.com>
2016-06-06 15:22:58 -0400

    Added some documentation

--- addon/mixins/scrollable.js
+++ addon/mixins/scrollable.js
@@ -50,9 +50,26 @@ export default Ember.Mixin.create({
   },
   
   actions: {
+    
+    /**
+     * Recalculate action should be called when size of the scroll area changes
+     */
     recalculate() {
       this._recalculateTrackpadScrollEmulator();
     },
+
+    /**
+     * Can be called when scrollbars changes as a result of value change, 
+     * 
+     * for example
+     * ```
+     * {{#as-scrollable as |scrollbars|}}
+     *   {{#each (compute (pipe scrollbars.update) rows) as |row|}}
+     *     {{row.title}}
+     *   {{/each}}
+     * {{/as-scrollable}}
+     * ```
+     */
     update(value) {
       if (this._didSetup) {
         scheduleOnce('afterRender', this, this._recalculateTrackpadScrollEmulator);        
