d049c97858ddfe1207379eaf222602f39ca474d2
Alex Alvarez <alexander-alvarez@github.com>
2016-12-29 13:14:58 -0500

    Tests for scrollbar component

--- tests/integration/components/ember-scrollbar-test.js
+++ tests/integration/components/ember-scrollbar-test.js
@@ -1,15 +1,385 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+import {moduleForComponent, test} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+
+const {$: jQuery} = Ember;
 
 moduleForComponent('ember-scrollbar', 'Integration | Component | ember scrollbar', {
   integration: true
 });
 
-/**
- * TODO TESTCASES
- *
- * - click on the scrollbar when not on handle causes jumping
- * - click and drag on scrollbar space moves the scrollbar (in vertical and horizontal mode), after mouseUp dragging stops
- * - click, move mouse off scrollbar and drag moves the scrollbar, after mouseup dragging should stop.
- *
- */
+const handleClass = '.drag-handle';
+const barClass = '.tse-scrollbar';
+
+test('Horizontal: offset and size get routed properly', function(assert) {
+  assert.expect(2);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      showHandle=true
+    }}
+    
+    </div>
+  </div>`);
+
+  // TODO no idea why offset and width are showing up as half of the set properties
+  // they look fine when I copy this code to the test app
+  assert.equal(this.$(handleClass).position().left, this.get('offset') / 2);
+  assert.equal(Number.parseInt(this.$(handleClass).css('width')), this.get('size') / 2);
+});
+
+test('Vertical: offset and size get routed properly', function(assert) {
+  assert.expect(2);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable vertical" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=false
+      showHandle=true
+    }}
+    
+    </div>
+  </div>`);
+
+  // TODO no idea why offset and width are showing up as half of the set properties
+  // they look fine when I copy this code to the test app
+  assert.equal(this.$(handleClass).position().top, this.get('offset') / 2);
+  assert.equal(Number.parseInt(this.$(handleClass).css('height')), this.get('size') / 2);
+});
+
+
+test('mousedown event on handle triggers startDrag, but not onJumpTo', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.on('onDragStart', function() {
+    assert.ok(true);
+  });
+  this.on('onJumpTo', function() {
+    assert.ok(false);
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      showHandle=true
+      onDragStart=(action 'onDragStart')
+      onJumpTo=(action 'onJumpTo')
+    }}
+    
+    </div>
+  </div>`);
+
+  this.$(handleClass).trigger('mousedown');
+});
+
+test('mousedown event on bar triggers onJumpTo and not startDrag', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+
+  this.on('onDragStart', function() {
+    assert.ok(false);
+  });
+
+  this.on('onJumpTo', function() {
+    assert.ok(true);
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      showHandle=true
+      onDragStart=(action 'onDragStart')
+      onJumpTo=(action 'onJumpTo')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN we mousedown on the bar and not the handle
+  this.$(barClass).trigger('mousedown');
+});
+
+
+test('Horizontal: onJumpTo has positive first argument when to the left of handle', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.on('onJumpTo', function(towardsAnchor) {
+    assert.ok(towardsAnchor, 'towardsAnchor should be true if going towards anchor');
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      showHandle=true
+      onJumpTo=(action 'onJumpTo')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN
+  const event = jQuery.Event("mousedown");
+  // TODO for some reason the realized offset is getting set to half of what is passed in during testing
+  // change offsetX to 5+ but < 10 and the test will fail because of this
+  event.offsetX = 2;
+  this.$(barClass).trigger(event);
+
+
+});
+
+test('Horizontal: onJumpTo has negative first argument when to the right of handle', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.on('onJumpTo', function(towardsAnchor) {
+    assert.notOk(towardsAnchor, 'towardsAnchor should be false if going towards away from anchor');
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      showHandle=true
+      onJumpTo=(action 'onJumpTo')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN
+  const event = jQuery.Event("mousedown");
+  event.offsetX = 30;
+  this.$(barClass).trigger(event);
+
+});
+
+
+test('Vertical: onJumpTo has positive first argument when to the top of handle', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.on('onJumpTo', function(towardsAnchor) {
+    assert.ok(towardsAnchor, 'towardsAnchor should be true if going towards anchor');
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable vertical" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=false
+      showHandle=true
+      onJumpTo=(action 'onJumpTo')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN
+  const event = jQuery.Event("mousedown");
+  // TODO for some reason the realized offset is getting set to half of what is passed in during testing
+  // change offsetY to 5+ but < 10 and the test will fail because of this
+  event.offsetY = 2;
+  this.$(barClass).trigger(event);
+
+
+});
+
+test('Vertical: onJumpTo has negative first argument when to the bottom of handle', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  this.on('onJumpTo', function(towardsAnchor) {
+    assert.notOk(towardsAnchor, 'towardsAnchor should be false if going away from anchor');
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable vertical" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=false
+      showHandle=true
+      onJumpTo=(action 'onJumpTo')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN
+  const event = jQuery.Event("mousedown");
+  event.offsetY = 30;
+  this.$(barClass).trigger(event);
+
+});
+
+
+test('mouseup event triggers onDragEnd', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10
+  });
+  let called = false;
+  this.on('onDragEnd', function() {
+    called = true;
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      showHandle=true
+      onDragEnd=(action 'onDragEnd')
+    }}
+    
+    </div>
+  </div>`);
+
+  this.$(handleClass).trigger('mouseup');
+
+  assert.ok(called);
+});
+
+
+test('Vertical: onDrag is called when a change occurs when onDragging is true and mouseOffset exists', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10,
+    isDragging: false
+  });
+
+  this.on('onDrag', function() {
+    // THEN
+    assert.ok(true);
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable vertical" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=false
+      dragOffset=30
+      mouseOffset=300
+      isDragging=isDragging
+      showHandle=true
+      onDrag=(action 'onDrag')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN
+  this.set('isDragging', true);
+
+});
+
+test('Horizontal: onDrag is called when a change occurs when onDragging is true and mouseOffset exists', function(assert) {
+  assert.expect(1);
+
+  this.setProperties({
+    size: 40,
+    offset: 10,
+    isDragging: false
+  });
+  this.on('onDrag', function() {
+    // THEN
+    assert.ok(true);
+  });
+
+  this.render(hbs`
+    <div style="height: 50px">
+    <div class="tse-scrollable horizontal" >
+
+    {{ember-scrollbar
+      handleOffset=offset
+      handleSize=size
+      horizontal=true
+      dragOffset=30
+      mouseOffset=300
+      isDragging=isDragging
+      showHandle=true
+      onDrag=(action 'onDrag')
+    }}
+    
+    </div>
+  </div>`);
+
+  // WHEN
+  this.set('isDragging', true);
+
+});
+
+// TODO verify that the drag percentage is calculated from mouse offset and drag offset and is a percentage between 0 and 1 of the scrollbar size
