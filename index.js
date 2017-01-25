1cccd6fed4a77ce01589e731b8f11f0d744c831d
offirgolan <offirgolan@gmail.com>
2017-01-25 14:43:21 -0800

    Remove unused version checker

--- index.js
+++ index.js
@@ -2,17 +2,8 @@
 
 'use strict';
 
-var VersionChecker = require('ember-cli-version-checker');
-
 module.exports = {
   name: 'ember-scrollable',
-
-  init: function() {
-    this._super.init && this._super.init.apply(this, arguments);
-
-    var checker = new VersionChecker(this);
-    this._checkerForEmber = checker.for('ember', 'bower');
-  },
 
   included: function(app) {
     while (app.app) {
