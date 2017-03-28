f261b1ce4e7875788267cfd1a26d85e7fa1a10ac
Jan Buschtöns <buschtoens@gmail.com>
2017-03-28 19:35:44 +0200

    build: include Babel polyfill
    Resolves IE freezing mentioned in #67

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    'ember-cli-babel': {
+      includePolyfill: true
+    },
     snippetSearchPaths: ['tests/dummy/app']
   });
 
