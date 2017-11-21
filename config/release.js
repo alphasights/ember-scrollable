c7e0aa10d2e2e08840662b3c7be851907f22d7c6
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-11-20 20:49:09 -0800

    Add eslint overrides for console statements

--- config/release.js
+++ config/release.js
@@ -1,4 +1,5 @@
 /* eslint-env node */
+/* eslint no-console: 0 */
 
 var execSync = require('child_process').execSync;
 var generateChangelog = require('ember-cli-changelog/lib/tasks/release-with-changelog');
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,3 +1,5 @@
+/* eslint no-console: 0 */
+
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
