03cee162cab3f0c2968c4b9b227a06ba848aedcd
Robert Wagner <rwwagner90@gmail.com>
2022-07-13 15:34:35 -0400

    Remove weird included hook

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-/* eslint-env node */
-
 'use strict';
 
 module.exports = {
   name: require('./package').name,
-
-  included(app) {
-    while (app.app) {
-      app = app.app;
-    }
-    this.app = app;
-
-    this._super.included.apply(this, arguments);
-  },
 };
