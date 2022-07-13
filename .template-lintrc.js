5a265a8c108b453bd60cc46a98597aeb2c18638f
Robert Wagner <rwwagner90@gmail.com>
2022-07-13 14:26:35 -0400

    Fix template lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,5 +4,9 @@ module.exports = {
   extends: 'recommended',
   rules: {
     'no-action': false,
+    'no-down-event-binding': false,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-yield-only': false,
   },
 };
